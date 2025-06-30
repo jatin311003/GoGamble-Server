0ed83dd93d804bbb2108b07ed492a1a1d6cf3dff
jatin311003 <ajatin3110@gmail.com>
2025-06-30 16:46:15 +0530

    fix: mongodb uri updated and using gogamble db

--- connection.js
+++ connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("MongoDB Atlas Connected"))
-.catch(err => console.log("DB Connection Error:", err));
+.then(() => console.log('MongoDB Atlas Connected'))
+.catch(err => console.error('DB Connection Error:', err));
